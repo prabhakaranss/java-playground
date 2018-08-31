96c78810576a02f995065061251828f34ea9bdde
prabhakaran ss <prabhakaran.ss@synchronoss.com>
2018-08-31 16:22:05 +0530

    renamed class

--- functional/src/PredicateSample.java
+++ functional/src/PredicateSample.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
-public class Predicate {
+public class PredicateSample {
 
   public static void main(String[] args){
     List<String> configKeyList = new ArrayList<>();
     configKeyList.add("first_level");
     configKeyList.add("second_level");
     configKeyList.add("three_level");
-    java.util.function.Predicate<String> shouldAConfigKey = (key) -> configKeyList.contains(key);
+    Predicate<String> shouldAConfigKey = (key) -> configKeyList.contains(key);
     System.out.println(shouldAConfigKey.test("name"));
     System.out.println(shouldAConfigKey.test("second_level"));
   }
