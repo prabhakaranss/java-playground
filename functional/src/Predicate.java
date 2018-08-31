28a37405110cc8b70ad1809a51de272111214843
prabhakaran ss <prabhakaran.ss@synchronoss.com>
2018-08-31 16:21:04 +0530

    predicate samples

--- functional/src/Predicate.java
+++ functional/src/Predicate.java
@@ -1,15 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
-public class Test {
+public class Predicate {
 
   public static void main(String[] args){
     List<String> configKeyList = new ArrayList<>();
     configKeyList.add("first_level");
     configKeyList.add("second_level");
     configKeyList.add("three_level");
-    Predicate<String> shouldAConfigKey = (key) -> configKeyList.contains(key);
+    java.util.function.Predicate<String> shouldAConfigKey = (key) -> configKeyList.contains(key);
     System.out.println(shouldAConfigKey.test("name"));
     System.out.println(shouldAConfigKey.test("second_level"));
   }
