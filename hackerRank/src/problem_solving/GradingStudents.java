77d815c577dd765f179e59adfb0e39a8ea3a6147
prabhakaran ss <prabhakaran.ss@synchronoss.com>
2019-05-16 11:16:52 +0530

    grading student problem

--- hackerRank/src/problem_solving/GradingStudents.java
+++ hackerRank/src/problem_solving/GradingStudents.java
@@ -2,4 +2,22 @@ package problem_solving;
 
 public class GradingStudents {
 
+  static int[] gradingStudents(int[] grades) {
+
+    int[] result = new int[grades.length];
+
+    for (int i = 0; i < grades.length; i++) {
+      if (grades[i] < 38) {
+        result[i] = grades[i];
+      } else {
+        int diff = grades[i] % 5;
+        if (diff > 2) {
+          result[i] = grades[i] + (5 - diff);
+        } else {
+          result[i] = grades[i];
+        }
+      }
+    }
+    return result;
+  }
 }
